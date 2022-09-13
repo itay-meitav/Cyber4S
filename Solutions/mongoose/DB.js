a4ab6c4d4073811080697e2704789fdb62d919f6
Itay Meitav <itai9999@gmail.com>
2022-09-13 23:47:20 +0300

    changing folder

--- Solutions/mongoose/DB.js
+++ Solutions/mongoose/DB.js
@@ -10,30 +10,30 @@ const AgentSchema = new mongoose.Schema({
 });
 const AgentModel = mongoose.model("agents", AgentSchema);
 
-// const csvFilePath = "./agents.csv";
-// const csv = require("csvtojson");
-// let c = 0;
-// csv()
-//   .fromFile(csvFilePath)
-//   .then(async (jsonObj) => {
-//     let agents = jsonObj.map((x) => {
-//       delete x.field7;
-//       if (x.dateReceived) {
-//         let date = x.dateReceived.split("/");
-//         let newDate = new Date(`${date[2]}/${date[1]}/${date[0]}`);
-//         x.dateReceived = newDate;
-//         // console.log("new", newDate);
-//         // console.log("old:", date.join("/"));
-//       } else {
-//         c++;
-//         x.dateReceived = new Date();
-//       }
-//       return x;
-//     });
-//     await AgentModel.insertMany(agents);
-//     mongoose.connection.close();
-//     console.log("count: ", c);
-//   });
+const csvFilePath = "./agents.csv";
+const csv = require("csvtojson");
+let c = 0;
+csv()
+  .fromFile(csvFilePath)
+  .then(async (jsonObj) => {
+    let agents = jsonObj.map((x) => {
+      delete x.field7;
+      if (x.dateReceived) {
+        let date = x.dateReceived.split("/");
+        let newDate = new Date(`${date[2]}/${date[1]}/${date[0]}`);
+        x.dateReceived = newDate;
+        // console.log("new", newDate);
+        // console.log("old:", date.join("/"));
+      } else {
+        c++;
+        x.dateReceived = new Date();
+      }
+      return x;
+    });
+    await AgentModel.insertMany(agents);
+    mongoose.connection.close();
+    console.log("count: ", c);
+  });
 
 module.exports.getCities = async function () {
   let cities = await AgentModel.find({}).distinct("city");
